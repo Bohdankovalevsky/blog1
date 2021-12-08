c8c7052084a458c3acc0a67954379e0adc75505e
Bohdan <bohdan.kovalevskyi@oa.edu.ua>
2021-12-08 12:29:17 +0200

    lab 10

--- app/Repositories/BlogCategoryRepository.php
+++ app/Repositories/BlogCategoryRepository.php
@@ -46,6 +46,7 @@ class BlogCategoryRepository extends CoreRepository
         $result = $this
             ->startConditions()
             ->select($columns)
+            ->with(['parentCategory:id,title',])
             ->paginate($perPage); //можна $columns додати сюди
 
         return $result;
